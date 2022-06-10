15bf881e2343d7f59486708710a1628b37d1da66
Julia <71259681+Stoian-Iulia@users.noreply.github.com>
2022-06-10 09:41:16 +0300

    Add files via upload

---
+++ task_8/project/src/components/HelloWorld.vue
@@ -0,0 +1,54 @@
+<template>
+  <div id="app">
+    <table class="table">
+      <thead>
+        <tr>
+          <td><strong>Utilizator</strong></td>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="row in rows" :key="row.name">
+          <td>{{ row.name }}</td>
+          <td>
+            <div class="remove_icon" @click="removeRow(row)">X</div>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+    <div>
+      Name:<input type="text" placeholder="Guest" v-model="name" />
+      <button class="button btn-primary" @click="addRow(name)">Add</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+  el: "#app",
+  data() {
+    return {
+      rows: [{ name: "Utilizator" }, { name: "Admin" }],
+    };
+  },
+  methods: {
+    addRow(name) {
+      this.rows.push({ name: name });
+    },
+    removeRow(row) {
+      const currIndex = this.rows.findIndex((item) => item.name === row.name);
+
+      this.rows = this.rows.filter((x, i) => i !== currIndex);
+      console.log(this.rows);
+    },
+  },
+};
+</script>
+
+<style scoped>
+@media (hover: hover) {
+  tr:hover {
+    background-color:#5af7b0;
+  }
+}
+</style>
